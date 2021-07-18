ae1c3065b7c973ff93144779374307fe7fe37911
ivVane <iv.vane@gmail.com>
2021-07-18 16:46:01 +0200

    Created Task entity class, ToDoDatabase.kt and TaskDao.kt

---
+++ app/src/main/java/com/vane/android/todoandroidtesting/data/source/local/ToDoDatabase.kt
@@ -0,0 +1,16 @@
+package com.vane.android.todoandroidtesting.data.source.local
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.vane.android.todoandroidtesting.data.Task
+
+/**
+ * The Room Database that contains the Task table.
+ *
+ * Note that exportSchema should be true in production databases.
+ */
+@Database(entities = [Task::class], version = 1, exportSchema = false)
+abstract class ToDoDatabase : RoomDatabase() {
+
+    abstract fun taskDao(): TaskDao
+}
