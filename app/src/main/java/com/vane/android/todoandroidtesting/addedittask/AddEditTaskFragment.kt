f6a779c1894344c6a6ad6f854f195824262548c9
ivVane <iv.vane@gmail.com>
2021-08-06 18:56:49 +0200

    Created and configured fragment_add_edit_task.xml and AddEditTaskFragment.kt

--- app/src/main/java/com/vane/android/todoandroidtesting/addedittask/AddEditTaskFragment.kt
+++ app/src/main/java/com/vane/android/todoandroidtesting/addedittask/AddEditTaskFragment.kt
@@ -1,8 +1,61 @@
 package com.vane.android.todoandroidtesting.addedittask
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import com.google.android.material.snackbar.Snackbar
+import com.vane.android.todoandroidtesting.EventObserver
 import com.vane.android.todoandroidtesting.R
+import com.vane.android.todoandroidtesting.databinding.FragmentAddEditTaskBinding
+import com.vane.android.todoandroidtesting.tasks.ADD_EDIT_RESULT_OK
+import com.vane.android.todoandroidtesting.util.setupRefreshLayout
+import com.vane.android.todoandroidtesting.util.setupSnackbar
 
-class AddEditTaskFragment : Fragment(R.layout.fragment_add_edit_task) {
+class AddEditTaskFragment : Fragment() {
+
+    private lateinit var viewDataBinding: FragmentAddEditTaskBinding
+
+    private val args: AddEditTaskFragmentArgs by navArgs()
+
+    private val viewModel by viewModels<AddEditTaskViewModel>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val root = inflater.inflate(R.layout.fragment_add_edit_task, container, false)
+        viewDataBinding = FragmentAddEditTaskBinding.bind(root).apply {
+            this.viewmodel = viewModel
+        }
+        // Set the lifecycle owner to the lifecycle of the view
+        viewDataBinding.lifecycleOwner = this.viewLifecycleOwner
+        return viewDataBinding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        setupSnackbar()
+        setupNavigation()
+        this.setupRefreshLayout(viewDataBinding.refreshLayout)
+        viewModel.start(args.taskId)
+    }
+
+    private fun setupSnackbar() {
+        view?.setupSnackbar(this, viewModel.snackbarText, Snackbar.LENGTH_SHORT)
+    }
+
+    private fun setupNavigation() {
+        viewModel.taskUpdatedEvent.observe(this, EventObserver {
+            val action = AddEditTaskFragmentDirections
+                .actionAddEditTaskFragmentToTasksFragment(ADD_EDIT_RESULT_OK)
+            findNavController().navigate(action)
+        })
+    }
 
 }
--- app/src/main/java/com/vane/android/todoandroidtesting/addedittask/AddEditTaskViewModel.kt
+++ app/src/main/java/com/vane/android/todoandroidtesting/addedittask/AddEditTaskViewModel.kt
@@ -15,7 +15,7 @@ import kotlinx.coroutines.launch
 /**
  * ViewModel for the Add/Edit screen.
  */
-class AddEditViewModel(application: Application) : AndroidViewModel(application) {
+class AddEditTaskViewModel(application: Application) : AndroidViewModel(application) {
 
     // Note, for testing and architecture purposes, it's bad practice to construct the repository
     // here. We'll show you how to fix this during the codelab
